eeadad4b64f4a073533df4305727f2eaeb6c5ff2
Hicf25 <166264982+Hicf25@users.noreply.github.com>
2024-04-07 02:11:32 -0400

    Add files via upload

---
+++ relaxingImages.py
@@ -0,0 +1,63 @@
+import streamlit as st
+
+# The app paassword is: pghv yiya kgyc alyv
+
+st.title("Relaxing images")
+st.divider()
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.image('images/img_1.jpeg')
+
+with col2:
+    st.image('images/img_2.jpeg')
+
+with col3:
+    st.image('images/img_3.jpeg')
+
+
+with col1:
+    st.image('images/img_4.jpeg')
+
+with col2:
+    st.image('images/img_5.jpeg')
+
+with col3:
+    st.image('images/img_6.jpeg')
+
+with col1:
+    st.image('images/img_7.jpeg')
+
+with col2:
+    st.image('images/img_8.jpeg')
+
+with col3:
+    st.image('images/img_9.jpeg')
+
+with col1:
+    st.image('images/img_10.jpeg')
+
+with col2:
+    st.image('images/img_11.jpeg')
+
+with col3:
+    st.image('images/img_12.jpeg')
+
+with col1:
+    st.image('images/img_13.jpeg')
+
+with col2:
+    st.image('images/img_14.jpeg')
+
+with col3:
+    st.image('images/img_15.jpeg')
+
+with col1:
+    st.image('images/img_16.jpeg')
+
+with col2:
+    st.image('images/img_17.jpeg')
+
+with col3:
+    st.image('images/img_18.jpeg')
